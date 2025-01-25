18074824053868687d0d0d910a1a98fafa36a3e2
hbchaney <hbchaney@gmail.com>
2025-01-25 11:10:09 -0500

    adjusted pwr settings

--- src/bin/timer.rs
+++ src/bin/timer.rs
@@ -5,7 +5,17 @@ use cortex_m_rt::entry;
 use panic_halt as _;
 
 use stm32g4xx_hal::{
-    adc::config::{self, Clock}, gpio::GpioExt, pac::{rcc::pllcfgr, Peripherals}, prelude::SetDutyCycle, pwm::PwmExt, pwr::PwrExt, rcc::{Config, PllConfig, PllMDiv, PllNMul, PllRDiv, PllSrc, RccExt}, stm32::TIM1, time::RateExtU32, timer::Timer
+    adc::config::{self, Clock},
+    gpio::GpioExt,
+    pac::{rcc::pllcfgr, Peripherals},
+    prelude::SetDutyCycle,
+    pwm::PwmExt,
+    pwr::PwrExt,
+    pwr::VoltageScale,
+    rcc::{Config, PllConfig, PllMDiv, PllNMul, PllRDiv, PllSrc, RccExt},
+    stm32::TIM1,
+    time::RateExtU32,
+    timer::Timer,
 };
 
 use rtt_target::{rprintln, rtt_init_print};
@@ -14,45 +24,51 @@ use rtt_target::{rprintln, rtt_init_print};
 fn main() -> ! {
     rtt_init_print!();
 
-    let dp = Peripherals::take().expect("cannot take peripherals"); 
-    let rcc = dp.RCC.constrain(); 
+    let dp = Peripherals::take().expect("cannot take peripherals");
+    let rcc = dp.RCC.constrain();
+    let pwr = dp
+        .PWR
+        .constrain()
+        .vos(VoltageScale::Range1 { enable_boost: true })
+        .freeze();
     let pll_conf = PllConfig {
         mux: PllSrc::HSI,
-            m: PllMDiv::DIV_4,
-            n: PllNMul::MUL_75,
-            r: Some(PllRDiv::DIV_2),
-            q: Some(stm32g4xx_hal::rcc::PllQDiv::DIV_2),
-            p: Some(stm32g4xx_hal::rcc::PllPDiv::DIV_2),
+        m: PllMDiv::DIV_4,
+        n: PllNMul::MUL_85,
+        r: Some(PllRDiv::DIV_2),
+        q: Some(stm32g4xx_hal::rcc::PllQDiv::DIV_2),
+        p: Some(stm32g4xx_hal::rcc::PllPDiv::DIV_2),
     };
 
-    let pll_conf = Config::pll().pll_cfg(pll_conf); 
-    let pwr = dp.PWR.constrain().freeze();
-    
-    let mut rcc = rcc.freeze(pll_conf, pwr); 
-    let gpioc = dp.GPIOC.split(&mut rcc); 
-    let pin = gpioc.pc3.into_alternate(); 
-    
-    let mut pwm = dp.TIM1.pwm(pin, 10.kHz(),&mut rcc); 
+    let pll_conf = Config::pll().pll_cfg(pll_conf);
 
-    rprintln!("Current APB2 value {}",rcc.clocks.sys_clk.to_MHz()); 
+    let mut rcc = rcc.freeze(pll_conf, pwr);
+    let gpioc = dp.GPIOC.split(&mut rcc);
+    let pin = gpioc.pc3.into_alternate();
 
-    //attempt to reconfig the timer 
+    let mut pwm = dp.TIM1.pwm(pin, 10.kHz(), &mut rcc);
+
+    rprintln!("Current APB2 value {}", rcc.clocks.sys_clk.to_MHz());
+
+    //attempt to reconfig the timer
     unsafe {
-        let tim = &(*TIM1::ptr()); 
+        let tim = &(*TIM1::ptr());
         //Channel 1
         //Disable the channel before configuring it
         tim.ccer.modify(|_, w| w.cc1e().clear_bit());
 
-        tim.ccmr1_output().modify(|_, w| w
-        //Preload enable for channel
-        .oc1pe().set_bit()
+        tim.ccmr1_output().modify(|_, w| {
+            w
+                //Preload enable for channel
+                .oc1pe()
+                .set_bit()
+                //Set mode for channel, the default mode is "frozen" which won't work
+                .oc1m()
+                .pwm_mode1()
+        });
 
-        //Set mode for channel, the default mode is "frozen" which won't work
-        .oc1m().pwm_mode1()
-        );
-
-        tim.arr.modify(|_, w| w.arr().bits(100-1));
-        tim.psc.modify(|_, w| w.psc().bits(140 - 1)); 
+        tim.arr.modify(|_, w| w.arr().bits(100 - 1));
+        tim.psc.modify(|_, w| w.psc().bits(140 - 1));
 
         //Enable the channel
         tim.ccer.modify(|_, w| w.cc1e().set_bit());
@@ -61,9 +77,9 @@ fn main() -> ! {
         tim.bdtr.modify(|_, w| w.moe().set_bit());
     }
 
-    pwm.set_duty_cycle(pwm.max_duty_cycle() / 4).unwrap(); 
+    pwm.set_duty_cycle(pwm.max_duty_cycle() / 4).unwrap();
     pwm.enable();
-    
+
     loop {
         cortex_m::asm::nop();
     }
